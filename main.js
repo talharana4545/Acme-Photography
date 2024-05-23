cea75355f1188d70f4d08f860a3f94fd4f97b43e
TALHA MAQBOOL <136365985+talharana4545@users.noreply.github.com>
2024-05-23 23:56:13 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+// animate smoth on scrool
+
+$('#view-work').on('click', function(){
+    const images  = $('#images').position().top;
+    $('html, body').animate(
+        {
+        scrollTop: images
+    },400);
+});
